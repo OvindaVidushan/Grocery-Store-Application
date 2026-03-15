07c93f518afd30e177ae1708718b567f9ebda682
Ovinda_Vidushan <ovindavidushan22@gmail.com>
2026-03-15 09:31:21 +0530

    updated code

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,10 +1,13 @@
+import datetime
 import mysql.connector
-__cnx=None  #global variable to hold the connection object
+
+__cnx = None
 
 def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='root',
-                              host='127.0.0.1',
-                              database='grocery_store')
-    return __cnx
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='root', database='grocery_store')
+
+  return __cnx
